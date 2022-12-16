7aeffe497f4fc87b79b96993e004a6f13c8951ad
MaksimMonastyrshin <Bubzuk_Ka@mail.ru>
2022-12-16 11:52:01 +1000

    002_task_2_Row_of_multiplications ready

--- 002_task_2_Row_of_multiplications/002_task_2_Row_of_multiplications.py
+++ 002_task_2_Row_of_multiplications/002_task_2_Row_of_multiplications.py
@@ -3,4 +3,16 @@
 # *Пример:*
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-n = int(input('Введите натуральное число: '))
+def mult(n):
+    if n == 1:
+        return 1
+    else:
+        return n * mult(n-1)
+
+number = int(input('Введите натуральное число: '))
+
+result_str = []
+for i in  range(1, number + 1):
+    result_str.append(mult(i))
+
+print(result_str)
